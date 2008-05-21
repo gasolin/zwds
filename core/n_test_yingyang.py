37f40cbc31cc29729d957c63773e12a43b3d96d5
gasolin <gasolin@9fd8390b-1c4d-0410-9986-e15b2f9a6154>
2008-05-21 01:22:36 +0000

    add test cases

---
+++ core/n_test_yingyang.py
@@ -0,0 +1,10 @@
+from n_yingyang import *
+
+def test_p_5929_5e72_8868_v():
+    assert p_4e01_v.p_9670_967d_v == p_9670_v
+    assert p_58ec_v.p_9670_967d_v == p_967d_v
+
+def test_p_5730_652f_8868_v():
+    assert p_672a_v.p_65b9_5411_v == p_5357_v
+    assert p_4e11_v.p_9670_967d_v == p_9670_v
+    assert p_7533_v.p_6642_9593_v == (15, 17)
